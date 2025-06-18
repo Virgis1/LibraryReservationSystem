573d439914d90fb286024cc5b13c9e315f60e718
Virginijus <zemaitisvirginijus@gmail.com>
2025-06-18 15:02:41 +0300

    Added description to Book.cs

--- LibraryReservationSystem/Business/Book.cs
+++ LibraryReservationSystem/Business/Book.cs
@@ -9,6 +9,7 @@ namespace LibraryReservationSystem.Business
     {
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Description { get; set; }
         public int Year { get; set; }
     }
 }
